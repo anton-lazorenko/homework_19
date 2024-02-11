557d0ac56366d982d2fc07e2b455a74f6e280b4a
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-02-11 19:39:24 +0200

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstName = '0', secondName = '0', thirdName = 'ami'
+console.log(firstName / secondName + thirdName)
+
+let aboutMe = {name: "Anton", nickname: "anton-lazorenko", phone: +380999999999, email: "html@js.net", sex: "Male", hobby: "fishing", stressResistance: true}
+let aboutMyFriend = {name: "Vlad", nickname: "Vlados", phone: +380666666666, email: "vlados@js.net", sex: "Male", hobby: "driving", stressResistance: false}
+let aboutScryptonite = {name: "Adil", nickname: "Scryptonite", phone: +760000000001, email: "scryp@tonite.grp", sex: "Male", hobby: "singing", stressResistance: true}
+let arr = [aboutMe, aboutMyFriend, aboutScryptonite]
+console.log(arr)
